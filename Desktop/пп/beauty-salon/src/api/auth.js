0c9018c27b49cb3cb962aab95337f6a8a9dc7ad4
PE69ITA <148531988+PE69ITA@users.noreply.github.com>
2025-03-19 02:45:25 +0600

    Delete Desktop directory

--- Desktop/пп/beauty-salon/src/api/auth.js
+++
@@ -1,9 +0,0 @@
-export const fakeLogin = ({ role }) => {
-    return new Promise(resolve => {
-      setTimeout(() => resolve({
-        success: true,
-        token: "fake-jwt-token",
-        role: role
-      }), 1000);
-    });
-  };
--- Desktop/пп/beauty-salon/src/components/admin/SearchAccounts.jsx
+++
@@ -1,9 +0,0 @@
-export default function SearchAccounts({ onSearch }) {
-    return (
-      <input 
-        type="text" 
-        placeholder="Поиск по email..." 
-        onChange={(e) => onSearch(e.target.value)}
-      />
-    );
-  }
